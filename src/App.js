84f6e406863a1750e3747d37f66d8db8bef6a484
unknown <an.dima@slexn.com>
2022-05-16 15:46:09 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const URL = 'http://localhost:8088/';
+
+  const fetchMovies = async () => {
+    const res = await fetch(`${URL}api/movie/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => response.json());
+    return await res;
+  };
+
+  const fetchTVs = async () => {
+    const res = await fetch(`${URL}api/tv/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => response.json());
+    return await res;
+  };
+
+  const fetchData = () => {
+    fetchMovies();
+    fetchTVs();
+  }
+
+  const addData = async () => {
+    const res = await fetch(`${URL}api/add`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => response.json());
+    return await res;
+  };
+    
+  const deleteData = async () => {
+    const res = await fetch(`${URL}api/delete`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => response.json());
+    return await res;
+  };
+  
+
+  const dropTable = async () => {
+    const res = await fetch(`${URL}api/drop`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => response.json());
+    return await res;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button className='button' onClick={deleteData}>Truncate table</button>
+      <button className='button' onClick={dropTable}>Drop table</button>
+      <button className='button' onClick={addData}>Add data</button>
+      <button className='button' onClick={fetchData}>
+        Get request
+      </button>
     </div>
   );
 }
